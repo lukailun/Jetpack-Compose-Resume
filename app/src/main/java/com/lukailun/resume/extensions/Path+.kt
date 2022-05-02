6fccc218c50ee0eaf73d23d14239e6172fc5f39f
Karen <957458041@qq.com>
2022-05-02 10:10:47 +0800

    feat: add Letters.json

--- app/src/main/java/com/lukailun/resume/extensions/Path+.kt
+++ app/src/main/java/com/lukailun/resume/extensions/Path+.kt
@@ -1,15 +1,50 @@
 package com.lukailun.resume.extensions
 
+import android.content.Context
 import androidx.compose.ui.graphics.Path
+import com.lukailun.resume.screens.launch.models.LetterStep
+import com.lukailun.resume.screens.launch.models.LetterStepKey
+import com.lukailun.resume.screens.launch.models.Letters
+import com.squareup.moshi.Moshi
 
-val Path.Companion.letterM: Path
-    get() = Path().apply {
-        this.moveTo(193.3F, 1.9F)
-        this.cubicTo(181.5F, 9.1F, 166.2F, 42.4F, 143.5F, 110F)
-        this.cubicTo(134.2F, 137.5F, 134.2F, 137.5F, 133.5F, 132.6F)
-        this.cubicTo(132.3F, 124.9F, 133.7F, 54F, 135.3F, 40F)
-        this.cubicTo(136.2F, 32.5F, 136.4F, 26.6F, 135.9F, 25.2F)
-        this.cubicTo(134.1F, 20.4F, 117.8F, 13.1F, 105.1F, 11.4F)
-        this.cubicTo(100.5F, 10.8F, 99.5F, 11F, 105.1F, 11.4F)
-        this.close()
+fun Path.Companion.M(context: Context): Path {
+    return Path().apply {
+        val inputStream = context.assets.open("Letters.json")
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(Letters::class.java)
+        val letters = adapter.fromJson(jsonString) ?: return@apply
+        letters.M.forEach { handle(it) }
     }
+}
+
+
+private fun Path.handle(step: LetterStep) {
+    when (step.key) {
+        LetterStepKey.POINT -> {
+            val params = step.params ?: return
+            moveTo(params.endPoint.x, params.endPoint.y)
+        }
+        LetterStepKey.CURVE -> {
+            val params = step.params ?: return
+            val controlPoint1 = params.controlPoint1 ?: return
+            val controlPoint2 = params.controlPoint2 ?: return
+            cubicTo(
+                controlPoint1.x,
+                controlPoint1.y,
+                controlPoint2.x,
+                controlPoint2.y,
+                params.endPoint.x,
+                params.endPoint.y
+            )
+        }
+        LetterStepKey.CURVE -> {
+            val params = step.params ?: return
+            lineTo(
+                params.endPoint.x,
+                params.endPoint.y
+            )
+        }
+        LetterStepKey.CURVE -> close()
+    }
+}
--- app/src/main/java/com/lukailun/resume/screens/launch/LaunchView.kt
+++ app/src/main/java/com/lukailun/resume/screens/launch/LaunchView.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.lukailun.resume.extensions.letterM
+import com.lukailun.resume.extensions.M
 import com.lukailun.resume.screens.launch.models.LetterStep
 import com.lukailun.resume.screens.launch.models.LetterStepKey
 import com.lukailun.resume.screens.launch.models.Letters
@@ -37,75 +37,13 @@ fun DefaultPreview() {
 
 @Composable
 fun messengerIcon() {
-    val colors = listOf(Color(0xFF02b8f9), Color(0xFF0277fe))
     val context = LocalContext.current
 
-    fun handle(path: Path, step: LetterStep) {
-        when (step.key) {
-            LetterStepKey.POINT -> {
-                val params = step.params ?: return
-                path.moveTo(params.endPoint.x, params.endPoint.y)
-            }
-            LetterStepKey.CURVE -> {
-                val params = step.params ?: return
-                val controlPoint1 = params.controlPoint1 ?: return
-                val controlPoint2 = params.controlPoint2 ?: return
-                path.cubicTo(
-                    controlPoint1.x,
-                    controlPoint1.y,
-                    controlPoint2.x,
-                    controlPoint2.y,
-                    params.endPoint.x,
-                    params.endPoint.y
-                )
-            }
-            LetterStepKey.CURVE -> {
-                val params = step.params ?: return
-                path.lineTo(
-                    params.endPoint.x,
-                    params.endPoint.y
-                )
-            }
-            LetterStepKey.CURVE -> path.close()
-        }
-    }
     Canvas(
         modifier = Modifier
             .size(100.dp)
-            .padding(16.dp)
     ) {
-
-//        val trianglePath = Path().let {
-//            it.moveTo(this.size.width * .20f, this.size.height * .77f)
-//            it.lineTo(this.size.width * .20f, this.size.height * 0.95f)
-//            it.lineTo(this.size.width * .37f, this.size.height * 0.86f)
-//            it.close()
-//            it
-//        }
-
-        val inputStream = context.assets.open("Letters.json")
-        val jsonString = inputStream.bufferedReader().use { it.readText() }
-        val moshi = Moshi.Builder().build()
-        val adapter = moshi.adapter(Letters::class.java)
-        val letters = adapter.fromJson(jsonString) ?: return@Canvas
-        val electricPath = Path().apply {
-            letters.M.forEach {
-                handle(this, it)
-            }
-        }
-//        val electricPath = Path.letterM
-
-        drawOval(
-            Brush.verticalGradient(colors = colors),
-            size = Size(this.size.width, this.size.height)
-        )
-
-//        drawPath(
-//            path = trianglePath,
-//            Brush.verticalGradient(colors = colors),
-//            style = Stroke(width = 15f, cap = StrokeCap.Round)
-//        )
-
+        val electricPath = Path.M(context)
         drawPath(path = electricPath, color = Color.Red)
     }
 }
