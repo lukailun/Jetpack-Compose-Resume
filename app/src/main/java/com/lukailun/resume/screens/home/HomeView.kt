699bedb611c27ea38579f6b13497da37a33076e6
lukailun <957458041@qq.com>
2023-01-19 15:08:50 +0800

    feat: add me svg

--- app/src/main/java/com/lukailun/resume/screens/home/HomeView.kt
+++ app/src/main/java/com/lukailun/resume/screens/home/HomeView.kt
@@ -1,5 +1,6 @@
 package com.lukailun.resume.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
@@ -8,10 +9,14 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
+import com.lukailun.resume.R
 import com.lukailun.resume.extensions.Pink
 import com.lukailun.resume.ui.theme.ResumeTheme
 
@@ -34,7 +39,17 @@ fun HomeView() {
                 ),
             ),
     ) {
-
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.weight(1F))
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.me),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.width(200.dp),
+                )
+            }
+        }
     }
 }
 
